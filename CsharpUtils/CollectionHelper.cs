06e7e69d5b1fa7d3a285055cdc376995fa6a47e8
Kenny Hung <kennysubs+github@outlook.com>
2012-12-16 13:31:35 +0000

    Beginnings of a solution.

---
+++ CsharpUtils/CollectionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpUtils
+{
+  public static class CollectionHelper
+  {
+    public static List<T> ToSingletonList<T>(this T singleton)
+    {
+      return new List<T> {singleton};
+    }
+  }
+}
---
+++ CsharpUtilsTest/CollectionHelperTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CsharpUtils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsharpUtilsTest
+{
+  [TestClass]
+  public class CollectionHelperTest
+  {
+    [TestMethod]
+    public void ToSingletonListWorks()
+    {
+      const String testString = "testString";
+      List<String> toTest = testString.ToSingletonList();
+      Assert.AreEqual(1, toTest.Count);
+      Assert.AreEqual(testString, toTest[0]);
+    }
+
+    [TestMethod]
+    public void ToSingletonListWorksWithNull()
+    {
+      List<String> toTest = (null as String).ToSingletonList();
+      Assert.AreEqual(1, toTest.Count);
+      Assert.IsNull(toTest[0]);
+    }
+  }
+}
