66f40ce6d19bc2ac2d2c94d35c6503bd54bd29e1
Kenny Hung <kennysubs+github@outlook.com>
2013-01-06 23:17:20 +0000

    String helper initial commit

---
+++ CsharpUtils/StringHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpUtils
+{
+  public static class StringHelper
+  {
+    public static string TruncateLastChars(this string source, int number)
+    {
+      return source.Substring(0, source.Length - number);
+    }
+  }
+}
---
+++ CsharpUtilsTest/StringHelperTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CsharpUtils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsharpUtilsTest
+{
+  [TestClass]
+  public class StringHelperTest
+  {
+    [TestMethod]
+    public void TruncateLastCharsWorks()
+    {
+      Assert.AreEqual("hell", "hellish".TruncateLastChars(3));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void TruncateLastThrowsWhenNumberOfCharsExceedsLength()
+    {
+      "hell".TruncateLastChars(50);
+    }
+  }
+}
